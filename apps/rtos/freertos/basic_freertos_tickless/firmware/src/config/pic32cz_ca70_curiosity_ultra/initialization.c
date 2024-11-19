70dd1698042de23322ac8d766f76fd7b77175f5a
Pranjal Das <pranjal.das@microchip.com>
2024-11-19 10:03:33 +0530

    [RTOS] Regenerated demo for rtos__freertos__basic_freertos_tickless__firmware__pic32cz_ca70_curiosity_ultra.X
    MH3-103126

--- apps/rtos/freertos/basic_freertos_tickless/firmware/src/config/pic32cz_ca70_curiosity_ultra/initialization.c
+++ apps/rtos/freertos/basic_freertos_tickless/firmware/src/config/pic32cz_ca70_curiosity_ultra/initialization.c
@@ -48,7 +48,6 @@
 #include "device.h"
 
 
-
 // ****************************************************************************
 // ****************************************************************************
 // Section: Configuration Bits
@@ -66,6 +65,12 @@
 // Section: Driver Initialization Data
 // *****************************************************************************
 // *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 7.2 - Deviation record ID - H3_MISRAC_2012_R_7_2_DR_1 */
+/* MISRA C-2012 Rule 11.1 - Deviation record ID - H3_MISRAC_2012_R_11_1_DR_1 */
+/* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+/* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+
 
 
 // *****************************************************************************
@@ -95,7 +100,7 @@
 // *****************************************************************************
 // *****************************************************************************
 
-
+/* MISRAC 2012 deviation block end */
 
 /*******************************************************************************
   Function:
@@ -110,6 +115,9 @@
 void SYS_Initialize ( void* data )
 {
 
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
+
 
     EFC_Initialize();
   
@@ -118,22 +126,20 @@ void SYS_Initialize ( void* data )
 
 
 
-	BSP_Initialize();
 	RSWDT_REGS->RSWDT_MR = RSWDT_MR_WDDIS_Msk;	// Disable RSWDT 
 
 	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
 
-
-
-
+	BSP_Initialize();
 
     APP_Initialize();
 
 
     NVIC_Initialize();
 
-}
 
+    /* MISRAC 2012 deviation block end */
+}
 
 /*******************************************************************************
  End of File
