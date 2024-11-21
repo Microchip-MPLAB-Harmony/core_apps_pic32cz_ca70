7d4aace7b7d88e443e820f165df7cf68441d66c5
Pranjal Das <pranjal.das@microchip.com>
2024-11-21 08:19:55 +0530

    [SYS_CONSOLE_DEBUG] Regenerated demo for system__console_debug__sys_console_debug_uart_read_write__firmware__pic32cz_ca70_curiosity_ultra_freertos.X
    MH3-103132

--- apps/system/console_debug/sys_console_debug_uart_read_write/firmware/src/config/pic32cz_ca70_curiosity_ultra_freertos/definitions.h
+++ apps/system/console_debug/sys_console_debug_uart_read_write/firmware/src/config/pic32cz_ca70_curiosity_ultra_freertos/definitions.h
@@ -48,14 +48,14 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "bsp/bsp.h"
+#include "peripheral/uart/plib_uart1.h"
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "system/console/sys_console.h"
 #include "system/console/src/sys_console_uart_definitions.h"
 #include "peripheral/efc/plib_efc.h"
-#include "peripheral/usart/plib_usart1.h"
+#include "bsp/bsp.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "system/int/sys_int.h"
@@ -74,8 +74,14 @@ extern "C" {
 #endif
 // DOM-IGNORE-END
 
+/* Device Information */
+#define DEVICE_NAME          "PIC32CZ2051CA70144"
+#define DEVICE_ARCH          "CORTEX-M7"
+#define DEVICE_FAMILY        "PIC32C"
+#define DEVICE_SERIES        "PIC32CZCA70"
+
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 300000000
+#define CPU_CLOCK_FREQUENCY 300000000U
 
 // *****************************************************************************
 // *****************************************************************************
