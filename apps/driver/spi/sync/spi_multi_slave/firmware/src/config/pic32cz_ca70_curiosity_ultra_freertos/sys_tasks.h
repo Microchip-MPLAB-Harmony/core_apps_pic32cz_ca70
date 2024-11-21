b041d7974fb43b1014abee77af3259e4cecdc143
Pranjal Das <pranjal.das@microchip.com>
2024-11-21 08:19:33 +0530

    [DRV_SPI] Regenerated demo for driver__spi__sync__spi_multi_slave__firmware__pic32cz_ca70_curiosity_ultra_freertos.X
    MH3-103066

--- apps/driver/spi/sync/spi_multi_slave/firmware/src/config/pic32cz_ca70_curiosity_ultra_freertos/sys_tasks.h
+++ apps/driver/spi/sync/spi_multi_slave/firmware/src/config/pic32cz_ca70_curiosity_ultra_freertos/sys_tasks.h
@@ -1,23 +1,23 @@
 /*******************************************************************************
-  User Configuration Header
+ System Tasks Header File
 
   File Name:
-    user.h
+    sys_tasks.h
 
   Summary:
-    Build-time configuration header for the user defined by this project.
+    This file contains declarations for task handles.
 
   Description:
-    An MPLAB Project may have multiple configurations.  This file defines the
-    build-time options for a single configuration.
+    Task handles declared in this header file can be used by the application
+    to control the behavior of the tasks.
 
   Remarks:
-    It only provides macro definitions for build-time configuration options
+    None
+ *******************************************************************************/
 
-*******************************************************************************/
 // DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2023 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -37,37 +37,36 @@
 * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
 * ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
 * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-*******************************************************************************/
+ *******************************************************************************/
 // DOM-IGNORE-END
 
-#ifndef USER_H
-#define USER_H
-
-#include "bsp/bsp.h"
-// DOM-IGNORE-BEGIN
-#ifdef __cplusplus  // Provide C++ Compatibility
-
-extern "C" {
-
-#endif
-// DOM-IGNORE-END
+#ifndef SYS_TASKS_H
+#define SYS_TASKS_H
 
 // *****************************************************************************
 // *****************************************************************************
-// Section: User Configuration macros
+// Section: Included Files
 // *****************************************************************************
 // *****************************************************************************
-#define LED_ON()                    LED1_On()
-#define LED_OFF()                   LED1_Off()
-#define LED_TOGGLE()                LED1_Toggle()
 
-//DOM-IGNORE-BEGIN
-#ifdef __cplusplus
-}
-#endif
-//DOM-IGNORE-END
+#include "configuration.h"
+#include "definitions.h"
 
-#endif // USER_H
-/*******************************************************************************
- End of File
-*/
+// *****************************************************************************
+// *****************************************************************************
+// Section: RTOS "Tasks" Handles
+// *****************************************************************************
+// *****************************************************************************
+/* Declaration of  APP_EEPROM1_Tasks task handle */
+extern TaskHandle_t xAPP_EEPROM1_Tasks;
+
+/* Declaration of  APP_EEPROM2_Tasks task handle */
+extern TaskHandle_t xAPP_EEPROM2_Tasks;
+
+/* Declaration of  APP_MONITOR_Tasks task handle */
+extern TaskHandle_t xAPP_MONITOR_Tasks;
+
+
+
+
+#endif //SYS_TASKS_H
