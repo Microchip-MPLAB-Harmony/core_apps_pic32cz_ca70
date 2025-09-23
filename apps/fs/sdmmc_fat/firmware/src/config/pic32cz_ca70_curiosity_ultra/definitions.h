55930a081066094509f0015fb2b0d4462d470ce2
Syam Nair <Syam.Nair@microchip.com>
2025-09-23 10:04:57 +0000

    [APP] Regenerated demo for fs__sdmmc_fat__firmware
    MH3C-327

--- apps/fs/sdmmc_fat/firmware/src/config/pic32cz_ca70_curiosity_ultra/definitions.h
+++ apps/fs/sdmmc_fat/firmware/src/config/pic32cz_ca70_curiosity_ultra/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -60,14 +60,14 @@
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/xdmac/plib_xdmac.h"
 #include "peripheral/efc/plib_efc.h"
-#include "peripheral/tc/plib_tc0.h"
 #include "bsp/bsp.h"
+#include "peripheral/tc/plib_tc0.h"
 #include "peripheral/hsmci/plib_hsmci.h"
-#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "system/time/sys_time.h"
 #include "app.h"
 
 
